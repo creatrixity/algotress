4174fd579480978d7469a396b3a6a3a8c3f485ef
creatrixity <creatrixity@gmail.com>
2019-02-07 16:00:34 +0100

    add module bundling with webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-cheap-module-source-map',
+
+  devServer: {
+      port: 8080,
+      contentBase: './dist'
+  },
+
+  entry: {
+      linkedList: './src/LinkedList/main.js'
+  },
+
+  mode: 'development',
+
+  plugins: [
+      new HTMLWebpackPlugin({
+          template: './src/index.html',
+          inject: true,
+          chunks: [],
+          filename: 'index.html'
+      }),
+      new HTMLWebpackPlugin({
+          template: './src/LinkedList/tmpl.html',
+          inject: true,
+          chunks: ['linkedList'],
+          filename: 'linked-list/index.html'
+      })
+  ]
+};
