79fb7146b1e20d9074cd6605ceac24c0bb386afc
creatrixity <creatrixity@gmail.com>
2019-02-12 09:44:03 +0100

    Add array implementation of a Queue.

--- src/Queue/index.js
+++ src/Queue/index.js
@@ -1,6 +1,6 @@
-import Queue from './LinkedListQueue';
+import Queue from './ArrayQueue';
 
-function test (){
+function testLinkedListQueue (){
   const costlyPlayers = new Queue();
   
   costlyPlayers.enqueue('Neymar da Silva Jnr');
@@ -15,5 +15,23 @@ function test (){
   costlyPlayers.printAll();
   console.log('The most expensive player here is', costlyPlayers.peek());  
 }
+
+function testArrayQueue (){
+  const bestPlayers = new Queue();
+  
+  bestPlayers.enqueue('Luka Modric');
+  bestPlayers.enqueue('Cristiano Ronaldo Aveiro dos Santos');
+  bestPlayers.enqueue('Lionel Andres Cuccitini Messi');
+  bestPlayers.enqueue('Ricardo "Kaka" Izecson Santos Leite');
+  bestPlayers.enqueue('Fabio Cannavaro');
+  bestPlayers.enqueue('Ronaldo de Assis Moeira "Ronaldinho"');
+  bestPlayers.enqueue('Luis Figo');
+  bestPlayers.enqueue('Ronaldo Nazario de Lima');
+  bestPlayers.enqueue('Zinedine Zidane');
+  
+  bestPlayers.printAll();
+}
+
+testArrayQueue();
 
 export default Queue;
